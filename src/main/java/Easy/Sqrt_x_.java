3612139fcf962eae3bed89b3fa657811a6c634d8
DUNHAOZHONG <dunhaozhong@gmail.com>
2017-11-11 12:10:35 -0500

    Calculate sqrt

---
+++ src/main/java/Easy/Sqrt_x_.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class Sqrt_x_ {
+    public int mySqrt(int x) {
+//Use Newton method
+//        int r = x;
+//        while(r > x/r){
+//            r = (r + x/r)/2;
+//        }
+//        return r;
+//====================================================
+//Use binary search
+//        if (x == 0)
+//            return 0;
+//        int left = 1, right = Integer.MAX_VALUE;
+//        while (true) {
+//            int mid = left + (right - left)/2;
+//            if (mid > x/mid) {
+//                right = mid - 1;
+//            } else {
+//                if (mid + 1 > x/(mid + 1))
+//                    return mid;
+//                left = mid + 1;
+//            }
+//        }
+//===================================================
+//Use statistics
+        if(x < 4) return x == 0 ? 0 : 1;
+        int res = 2 * mySqrt(x/4);
+        if((res+1) * (res+1) <= x && (res+1) * (res+1) >= 0) return res+1;
+        return res;
+    }
+}
---
+++ src/test/java/Easy/Sqrt_x_Test.java
@@ -0,0 +1,15 @@
+package Easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sqrt_x_Test {
+    @Test
+    public void mySqrt() throws Exception {
+        Sqrt_x_ sqrt_x_ = new Sqrt_x_();
+        for(int i=5; i<18; i++)
+            System.out.println("sqrt("+i+"): " + sqrt_x_.mySqrt(i));
+    }
+
+}
