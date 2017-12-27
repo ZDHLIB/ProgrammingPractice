7ff713fd96312e7de289ea38ba96509330513998
QU66Q <dunhaozhong@gmail.com>
2017-12-27 12:35:41 -0500

    Add new easy leecode

---
+++ src/test/java/Easy/Q190Test.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q190Test {
+    @Test
+    public void reverseBits() throws Exception {
+        Q190 q190 = new Q190();
+        q190.reverseBits(1);
+    }
+
+}
---
+++ src/test/java/Easy/Q371Test.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q371Test {
+    @Test
+    public void getSum() throws Exception {
+        Q371 q371 = new Q371();
+        System.out.println(q371.getSum(1,2));
+    }
+
+}
