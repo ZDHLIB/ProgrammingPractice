6dc6f21b00cbc2d30d34fa379d15e3bb9b5ca759
DUNHAO ZHONG <dunhaozhong@gmail.com>
2018-01-11 18:09:03 -0500

    The longest common prefix of a group of strings, using TrieTree

---
+++ src/test/java/Easy/Q14Test.java
@@ -0,0 +1,18 @@
+package Easy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Q14Test {
+    @Test
+    public void longestCommonPrefix() throws Exception {
+        Q14 q14 = new Q14();
+        String[] a = new String[]{
+                "a",
+                "a"
+        };
+        System.out.println(q14.longestCommonPrefix(a));
+    }
+
+}
