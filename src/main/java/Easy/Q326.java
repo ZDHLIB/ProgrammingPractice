d77751c7fc0b34ad758314076e918258df4581af
DUNHAO ZHONG <dunhaozhong@gmail.com>
2018-01-28 16:07:12 -0500

    3 easy leetcode

---
+++ src/main/java/Easy/Q326.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class Q326 {
+    public boolean isPowerOfThree(int n) {
+        if(n < 1) return false;
+        while(n % 3 == 0) n /= 3;
+        return n == 1;
+    }
+
+    public boolean isPowerOfThree2(int n) {
+        return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+    }
+
+    public boolean isPowerOfThree3(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}
